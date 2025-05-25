e9a092d6c14abc779ce07b7054407a98b0f0deae
PrathamSingh1 <prathamjii36@gmail.com>
2025-05-25 16:38:38 +0000

    backend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,8 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    // const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://code-reviewer-backend-one.vercel.app/ai/get-review', { code })
     setReview(response.data)
   }
 
